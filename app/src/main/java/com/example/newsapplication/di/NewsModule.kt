db85df3dc8ca90b9ccfa2e2a6d1a2cbe1cacc971
vishesh211 <visheshtiwari211@gmail.com>
2023-04-12 19:40:43 +0530

    feat: created NewsModule.kt

---
+++ app/src/main/java/com/example/newsapplication/di/NewsModule.kt
@@ -0,0 +1,23 @@
+package com.example.newsapplication.di
+
+import com.example.newsapplication.api.NewsApi
+import com.example.newsapplication.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+object NewsModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(NewsApi::class.java)
+}
--- app/src/main/java/com/example/newsapplication/utils/Constants.kt
+++ app/src/main/java/com/example/newsapplication/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.newsapplication.utils
 object Constants {
     const val COUNTRY = "in"
     const val API_KEY = "35e96e729a6848a48c1c31aaf412b671"
+    const val BASE_URL = "https://newsapi.org"
 }
