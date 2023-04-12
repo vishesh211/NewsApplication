326251c399e46b666807cf9d8cbc43341451188c
vishesh211 <visheshtiwari211@gmail.com>
2023-04-12 19:43:04 +0530

    feat: added return type

--- app/src/main/java/com/example/newsapplication/api/NewsApi.kt
+++ app/src/main/java/com/example/newsapplication/api/NewsApi.kt
@@ -1,7 +1,9 @@
 package com.example.newsapplication.api
 
+import com.example.newsapplication.dataclasses.GetTopNewsHeadlines
 import com.example.newsapplication.utils.Constants.API_KEY
 import com.example.newsapplication.utils.Constants.COUNTRY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,5 +14,5 @@ interface NewsApi {
     suspend fun getTopNewsHeadlines(
         @Query("country") country: String = COUNTRY,
         @Query("apiKey") api_key: String = API_KEY
-    )
+    ): Response<GetTopNewsHeadlines>
 }
